215d30d0059c2129e5474944a1b2bfd5c3272b4d
kuchtak <k.kuchta@bioinfo.pl>
2021-11-27 16:40:01 +0100

    Adding testnet data.

--- contracts/RoxPresale/RoxPresale.sol
+++ contracts/RoxPresale/RoxPresale.sol
@@ -13,22 +13,16 @@ import "./SafeMath.sol";
 
 abstract contract Globals {
     // testnet data
-    //address constant public BSC_USD_ADDRESS = 0x4a0Ce8465D187290e4f51Ced1e114F2B1d96b709;
-    //address constant public BUSD_ADDRESS = 0x483304c20C270e9ccED7A7C9d0daCaEFf842833A;
-    //address constant public TUSD_ADDRESS = 0x40B70563cC795941f478892fF475FcB358040e90;
-    //IERC20 constant public ROX_TOKEN = ERC20(0xa7BdA6Ee7DD9D26DF780470ec71B480BF70Fe10D);
+    address constant public BSC_USD_ADDRESS = 0x4a0Ce8465D187290e4f51Ced1e114F2B1d96b709;
+    address constant public BUSD_ADDRESS = 0x483304c20C270e9ccED7A7C9d0daCaEFf842833A;
+    address constant public TUSD_ADDRESS = 0x40B70563cC795941f478892fF475FcB358040e90;
+    IERC20 constant public ROX_TOKEN = ERC20(0xa7BdA6Ee7DD9D26DF780470ec71B480BF70Fe10D);
 
     // production data
     //address constant public BSC_USD_ADDRESS = 0x55d398326f99059fF775485246999027B3197955;
     //address constant public BUSD_ADDRESS = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
     //address constant public TUSD_ADDRESS = 0x14016E85a25aeb13065688cAFB43044C2ef86784;
     //IERC20 constant public ROX_TOKEN = ERC20(0xf921758DA283e166ED13312C0b92e21BDeFD82F2);
-
-    // test data
-    address constant public BSC_USD_ADDRESS = 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8;
-    address constant public BUSD_ADDRESS = 0xf8e81D47203A594245E36C48e151709F0C19fBe8;
-    address constant public TUSD_ADDRESS = 0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B;
-    IERC20 constant public ROX_TOKEN = ERC20(0xd9145CCE52D386f254917e481eB44e9943F39138);
 }
 
 
